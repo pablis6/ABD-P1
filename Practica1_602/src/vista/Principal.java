e5892dad1b4601de925226006c4430fda0bd4d88
pablis6 <pablofergra@hotmail.com>
2015-03-05 00:57:37 +0100

    correccion de errores menores

--- Practica1_602/src/vista/Principal.java
+++ Practica1_602/src/vista/Principal.java
@@ -1,17 +1,33 @@
 package vista;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
 import java.awt.Color;
 import java.awt.Font;
-import javax.swing.JTabbedPane;
+import java.awt.GridBagConstraints;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
 import javax.swing.JTable;
+import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.JButton;
 
 public class Principal extends JFrame{
+	private static DefaultTableModel tableModel;
 	private JTable table;
+	private JLabel lblNick, lblAnios, lblPuntos, lblAvatar;
+	private JTabbedPane tabbedPane;
+	private JPanel tabCrucigrama, tabAmigos, tabPeticiones;
+	private Object[][] contenido;
+	private String[] nombreColumnas;
+	private JScrollPane sc;
+	private JButton btnAniadir;
+	private JButton btnEliminar;
+	private JButton btnDescartar;
+	
 	public Principal() {
 		getContentPane().setLayout(null);
 		
@@ -19,57 +35,57 @@ public class Principal extends JFrame{
     	this.setSize(370, 460);
     	this.setResizable(false);
 		
-		JLabel lblNick = new JLabel("New label");
+		lblNick = new JLabel("New label");
 		lblNick.setFont(new Font("Tahoma", Font.BOLD, 11));
 		lblNick.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNick.setBounds(93, 11, 178, 14);
 		getContentPane().add(lblNick);
 		
-		JLabel lblAnios = new JLabel("New label");
+		lblAnios = new JLabel("New label");
 		lblAnios.setHorizontalAlignment(SwingConstants.CENTER);
 		lblAnios.setBounds(93, 36, 178, 14);
 		getContentPane().add(lblAnios);
 		
-		JLabel lblPuntos = new JLabel("New label");
+		lblPuntos = new JLabel("New label");
 		lblPuntos.setFont(new Font("Tahoma", Font.BOLD, 11));
 		lblPuntos.setForeground(Color.ORANGE);
 		lblPuntos.setHorizontalAlignment(SwingConstants.CENTER);
 		lblPuntos.setBounds(93, 61, 178, 14);
 		getContentPane().add(lblPuntos);
 		
-		JLabel lblAvatar = new JLabel("New label");
+		lblAvatar = new JLabel("New label");
 		lblAvatar.setBounds(35, 11, 50, 64);
 		getContentPane().add(lblAvatar);
 		
-		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.setBounds(10, 86, 344, 335);
 		getContentPane().add(tabbedPane);
 		
-		JPanel tabCrucigrama = new JPanel();
+		tabCrucigrama = new JPanel();
 		tabbedPane.addTab("Crucigrama", null, tabCrucigrama, null);
 		
-		table = new JTable();
-		table.setModel(new DefaultTableModel(
-			new Object[][] {
-				{null, null},
-			},
-			new String[] {
-				"Título", "Fecha"
-			}
-		) {
-			Class[] columnTypes = new Class[] {
-				String.class, String.class
-			};
-			public Class getColumnClass(int columnIndex) {
-				return columnTypes[columnIndex];
-			}
-		});
-		tabCrucigrama.add(table);
+		JButton btnAbrir = new JButton("Abrir crucigrama");
+		tabCrucigrama.add(btnAbrir);
 		
-		JPanel tabAmigos = new JPanel();
+		JButton btnBusqueda = new JButton("busqueda de crucigramas");
+		tabCrucigrama.add(btnBusqueda);
+		
+		tabPeticiones = new JPanel();
+		tabbedPane.addTab("Peticiones de ayuda", null, tabPeticiones, null);
+		
+		JButton btnAceptar = new JButton("Abrir crucigrama");
+		tabPeticiones.add(btnAceptar);
+		
+		btnDescartar = new JButton("Descartar peticion");
+		tabPeticiones.add(btnDescartar);
+		
+		tabAmigos = new JPanel();
 		tabbedPane.addTab("Amigos", null, tabAmigos, null);
 		
-		JPanel tabPeticiones = new JPanel();
-		tabbedPane.addTab("Peticiones de ayuda", null, tabPeticiones, null);
+		btnAniadir = new JButton("Añadir amigo");
+		tabAmigos.add(btnAniadir);
+		
+		btnEliminar = new JButton("Eliminar");
+		tabAmigos.add(btnEliminar);
 	}
 }
